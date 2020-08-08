ddb78144f02b790c6349f7b9df983b95f07683e6
TheGeekonQuality <69384279+TheGeekonQuality@users.noreply.github.com>
2020-08-08 14:50:53 +0530

    Add files via upload

---
+++ src/main/java/com/way2automation/common/LocatorsForTests.java
@@ -0,0 +1,24 @@
+package com.way2automation.common;
+
+public class LocatorsForTests {
+
+	public static String navigationListXpath = "//*[@id='toggleNav']/li";
+	public static String registrationFieldsXpath = "//*[@id='load_box']//fieldset//input";
+	public static String registrationPageSubmitXpath = "//*[@id='load_box']//input[@class='button']";
+	public static String registrationPageSigninXpath = "//*[@id='load_box']//a";
+	public static String registrationTabXpath = "//*[@id='toggleNav']/li[6]";
+	
+	public static String signInXpaths = "//*[@id='login']//fieldset//input";
+	public static String signInButtonXpath = "//*[@id='login']//input[@class='button']";
+	
+	public static String dropDownXpath = "//*[@id='wrapper']/div[2]/div[2]/div[4]/ul/li[2]/a/figure/img";
+	public static String selectCountryXpath = "/html/body/select";
+	public static String selectCountry2Xpath = "//*[@id='combobox']";
+	public static String dropdownTabsXpath = "//*[@class='responsive-tabs']/li[2]/a";
+	public static String enterCountryXpath = "/html/body/div[1]/span/input";
+	
+	public static String WidgetTabsXpath = "//*[@id='wrapper']/div[2]/div[2]/div[2]/ul/li[6]/a/figure";
+	public static String WidgetDatePickerXpath = "//*[@id='wrapper']/div[2]/div[2]/div[2]/ul/li[3]/a/figure";
+	public static String AlertsXpath = "//*[@id='wrapper']/div[2]/div[2]/div[6]/ul/li/a/figure";
+	public static String FramesandWindowsXpath = "//*[@id='wrapper']/div[2]/div[2]/div[3]/ul/li/a/figure";
+}
