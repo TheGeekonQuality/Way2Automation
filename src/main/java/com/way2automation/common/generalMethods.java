bdb9768511700c2bcee69bfbe20fbfe81ec77894
TheGeekonQuality <69384279+TheGeekonQuality@users.noreply.github.com>
2020-08-08 14:46:08 +0530

    Add files via upload

---
+++ src/main/java/com/way2automation/common/generalMethods.java
@@ -0,0 +1,29 @@
+package com.way2automation.common;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class generalMethods {
+
+	private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	public static void waitForAjax(WebDriver driver) {
+		new WebDriverWait(driver, 180).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver driver) {
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				return (Boolean) js.executeScript("return jQuery.active == 0");
+			}
+		});
+	}
+
+	public static String randomAlphaNumeric(int count) {
+		StringBuilder builder = new StringBuilder();
+		while (count-- != 0) {
+			int character = (int) (Math.random() * ALPHA_NUMERIC_STRING.length());
+			builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+		}
+		return builder.toString();
+	}
+}
